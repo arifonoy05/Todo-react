bd8470d732edf82a5e8a3fba8aa2e208b745f651
Arif Onoy <arifonoy05@gmail.com>
2020-01-17 23:50:31 +0600

    Add files via upload

---
+++ precache-manifest.d805a914a8d3ef27eb8667121f8f68ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e48435f47c86e32ba6e879dbaaf3fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a1baa5005ebd63e071",
+    "url": "/static/css/main.1955d5d9.chunk.css"
+  },
+  {
+    "revision": "b91ff5b1b4e78c08997a",
+    "url": "/static/js/2.a193cc26.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a193cc26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25a1baa5005ebd63e071",
+    "url": "/static/js/main.cc995fab.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
